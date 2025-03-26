95acfaa77889655f3409131457c36d7296215e44
Chano Huaman Landa <shanohl.sist@gmail.com>
2025-03-25 23:35:13 -0500

    feat: remove docs

--- Readme.md
+++ Readme.md
@@ -41,4 +41,3 @@ POST `/buscar`
 }
 ```
 
-# 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
--- test.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(f"¿CUDA disponible?: {torch.cuda.is_available()}")
-print(f"GPU detectada: {torch.cuda.get_device_name(0)}")
-print(f"Versión CUDA: {torch.version.cuda}")
